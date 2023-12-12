0348351f079e319c8dd6f623adc48ec6576bdb18
Fyrstikkeske <79742888+Fyrstikkeske@users.noreply.github.com>
2023-12-12 20:13:27 +0100

    roterende KUBE, LESSFUCKINGOOOO!!
    rotert kameraet med et uhell en stund

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,40 @@
 use bevy::prelude::*;
 
+#[derive(Component)]
+struct værsåsnillogroter {}
+
+
 fn main(){
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
+        .add_systems(Update, rotate_cube)
         .run();
 }
+
+
+fn rotate_cube(
+    mut query: Query<&mut Transform,With<værsåsnillogroter>>
+){
+    for mut transform in query.iter_mut(){
+        transform.rotate_y(0.001);
+    }
+}
+
 
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    commands.spawn(PbrBundle {
+    commands.spawn((PbrBundle {
         mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
         material: materials.add(Color::rgb_u8(124, 144, 255).into()),
-        transform: Transform::from_xyz(0.0, 0.5, 0.0),
+        transform: Transform::from_xyz(0.0, 0.0, 0.0),
         ..default()
-    });
+    },
+        værsåsnillogroter{}
+    ));
     // light
     commands.spawn(PointLightBundle {
         point_light: PointLight {
